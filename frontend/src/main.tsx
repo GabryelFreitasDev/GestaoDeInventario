c3e28c642a9b24c88f187a37b12f22b73901176b
Leonardo de Meira Oliveira <106771518+LeonardX7@users.noreply.github.com>
2024-10-24 01:04:15 -0300

    Adicionando protótipo da tela de cadastro de produtos

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
+import { CadastroProdutos } from './pages/Produtos/cadastroProdutos'
 import './index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <CadastroProdutos />
   </React.StrictMode>,
 )
