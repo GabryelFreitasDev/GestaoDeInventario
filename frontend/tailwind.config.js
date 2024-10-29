f8a9c8c4e7c5010271697d67b24b21459efccb4e
GabrielNeja <gsneja@minha.fag.edu.br>
2024-10-28 21:54:27 -0300

    Criado sidebar

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,35 +1,13 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	mode: "jit",
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
 	  extend: {
 		colors: {
-		  gray: {
-			50: "#F8FAFC",
-			100: "#F1F5F9",
-			200: "#E2E8F0",
-			300: "#CBD5E1",
-			400: "#94A3B8",
-			500: "#64748B",
-			600: "#475569",
-			700: "#334155",
-			800: "#1E293B",
-			900: "#0F172A",
-		  },
-		  purple: {
-			400: "#9F67FF",
-			500: "#7C3AED",
-		  },
-		  white: "#ffffff",
-		  red: "#ED3A5A",
-		},
-		fontFamily: {
-		  sans: ["Titillium Web", "sans-serif"],
-		},
-		backgroundImage: {
-		  "img-purple": "url('/src/assets/bg.svg')",
+		  "dark-purple": "#081A51",
+		  "light-white": "rgba(255,255,255,0.17)",
 		},
 	  },
 	},
 	plugins: [],
-  }
+  };
